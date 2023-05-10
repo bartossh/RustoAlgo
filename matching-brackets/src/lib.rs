a4d055bc53304df3e27e2022f691976a4ac6e260
Bartosz Lenart <lenart.consulting@gmail.com>
2023-05-10 07:38:37 +0000

    Solved matching branckets exercism.

---
+++ matching-brackets/src/lib.rs
@@ -0,0 +1,39 @@
+pub fn brackets_are_balanced(string: &str) -> bool {
+    let mut queue: Vec<char> = Vec::new();
+
+    for c in string.chars() {
+        match c {
+            '{' => queue.push(c),
+            '}' => {
+                if !is_match(&'{', &mut queue) {
+                    return false;
+                }
+            }
+            '(' => queue.push(c),
+            ')' => {
+                if !is_match(&'(', &mut queue) {
+                    return false;
+                }
+            }
+            '[' => queue.push(c),
+            ']' => {
+                if !is_match(&'[', &mut queue) {
+                    return false;
+                }
+            }
+            _ => (),
+        }
+    }
+
+    queue.len() == 0
+}
+
+fn is_match(c: &char, queue: &mut Vec<char>) -> bool {
+    if let Some(v) = queue.pop() {
+        if v != *c {
+            return false;
+        }
+        return true;
+    }
+    false
+}
